60d7dbce6ffa9e9cc5ad439ed66f6327a4e1ee82
Huawei <meelo.k@mail.ru>
2021-11-29 16:58:16 +0300

    Initial commit

---
+++ src/test/java/pro/sky/java/course2/shoppingservice/ShoppingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.shoppingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
